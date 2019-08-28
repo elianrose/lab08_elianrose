a058dfc9895dff5f41015f32450822117509a87c
Tingwei Li <tingwei_li@ucsb.edu>
2019-08-28 01:37:33 -0700

    Initial version

---
+++ recLinkedListFuncs.cpp
@@ -0,0 +1,29 @@
+#include "linkedList.h"
+#include "linkedListFuncs.h"
+
+
+//head: ptr to a Node * which is the head of a linked list
+//return sum of all values in linked list using a recursive approach
+//if head is null return 0
+int recursiveSum(Node* head) {
+  if(head == NULL){
+	return 0;
+  }else{
+	return recursiveSum(head -> next) + head -> data;
+  }
+}
+
+//head: ptr to a Node* which is the head of a linked list
+//return the largest value in the linked list using a recursive approach
+//you may assume the list has at least one element
+int recursiveLargestValue(Node* head) {
+  Node* k = head;
+  if(head == NULL){
+	return 0;
+  }else{
+	if (recursiveLargestValue(head -> next) > head -> data){
+		k = recursiceLargestValue(head -> next);
+	}
+  }
+  return k;
+}
---
+++ strFuncs.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include "strFuncs.h"
+using namespace std;
+
+
+/* Precondition: Two valid strings s1 and s2, each containing a mix of alphabets, spaces and punctuations
+ * Post condition: Return true if one string is an anagram of the other string. White spaces, punctuations and
+ * the case for the letters (upper or lower) should not
+ * affect your result. 
+ */
+
+bool isAnagram(string s1, string s2){
+  string new_s1 = "";
+  string new_s2 = "";
+  for(int i = 0; i < s1.length();i++){
+	if(isalpha(s1[i])){
+		new_s1 += tolower(s2[i]);
+	}
+  }
+  for(int j = 0l h < s2.length();j++){
+	if(isalpha(s2[j])){
+		new_s2 += tolower(s2[j]);
+	}
+  }
+ 
+  sort(new_s1.begin(),new_s2.end());
+  sort(new_s2.begin(),new_s2.end());
+  return(new_s1 == new_s2);
+}
+
+
+
+
+
+
+
+
+/* Precondition: s1 is a valid string that may contain upper or lower case alphabets, no spaces or special characters
+ * Postcondition: Returns true if s1 is a palindrome, false otherwise
+ *You should provide a recursive solution*/
+bool isPalindrome(const string s1){
+	int length = s1.length();
+	string new_s1 = "";
+	for(int i = 0; i < length; i++){
+		new_s1 += tolower(s1[length-j-1])；
+	}
+	return (new_s1==s1);
+  
+}
+
+
